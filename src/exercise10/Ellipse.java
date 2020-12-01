6071b7a5a60ac5d8a2941a96bd29c320c1239697
MarioMarkov <mario11septemvri@gmail.com>
2020-12-01 16:41:14 +0000

    2

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -45,6 +45,13 @@ public class Ellipse  extends Figure {
     @Override
     public boolean containsClick(Point click) {
         // Check if click point is inside the rectangle
-        return false;
+        double clickX = click.getX();
+        double clickY = click.getY();
+        double centerX = startPoint.getX()+side1;
+        double centerY = startPoint.getY()+side2;
+         double part1 =(Math.pow(clickX-centerX,2))/(side1*side1);
+         double part2 =(Math.pow(clickY-centerY,2))/(side2*side2);
+
+        return part1 + part2 <=1;
     }
 }
